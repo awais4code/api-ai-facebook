fd898fad28e5681eaa1aacc598d4ca956e182836
Muhammad Awais <jxnenterprises@gmail.com>
2016-07-21 18:38:22 +0500

    changes

--- src/app.js
+++ src/app.js
@@ -213,15 +213,15 @@ function processEvent(event) {
                     }else{
                         if(isMathEq("3x+5=10")){
                             console.log("Math Eq");
-                            
-                            let url = "https://eimi.io/wolfram/samples/simpleRequest.php?q="+encodeURIComponent(resolvedQuery)+"";
-                            console.log("Url",url);
 
-                            requestify.get("https://eimi.io/wolfram/samples/simpleRequest.php?q=3x%2B5%3D6")
-                            .then(function(response) {
-                                response = response.getBody();
-                                sendFBMessage(sender,{text: response});  
-                            });
+                            let url = "https://eimi.io/wolfram/samples/simpleRequest.php?q="+encodeURIComponent(resolvedQuery)+"";
+                            sendFBMessage(sender,{text: url});
+
+                            // requestify.get("https://eimi.io/wolfram/samples/simpleRequest.php?q=3x%2B5%3D6")
+                            // .then(function(response) {
+                            //     response = response.getBody();
+                            //     sendFBMessage(sender,{text: response});  
+                            // });
                         }else{
                             console.log("In Else");
                             let splittedText = splitResponse(responseText);
