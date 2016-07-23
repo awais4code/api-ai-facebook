3d6ca7f698905fada33d54488bf710d2f8d567d1
Muhammad Awais <jxnenterprises@gmail.com>
2016-07-23 23:17:36 +0500

    changes

--- src/app.js
+++ src/app.js
@@ -272,16 +272,11 @@ function processEvent(event) {
                         let imgUrl = "https://eimi.io/img/Search_World.jpg";
                         sendFBTemplateMessage(sender,webUrl,subtitle,imgUrl,"Get Bread");
                     }else{
-                        if(responseText.length>0){
-                            let splittedText = splitResponse(responseText);
+                        let splittedText = splitResponse(responseText);
 
-                            async.eachSeries(splittedText, (textPart, callback) => {
-                                sendFBMessage(sender, {text: textPart}, callback);
-                            });
-                        }else{
-                            let respText = "I'm still learning about that myself. As soon as I know, you'll know.";
-                            sendFBMessage(sender, {text: respText});
-                        }
+                        async.eachSeries(splittedText, (textPart, callback) => {
+                            sendFBMessage(sender, {text: textPart}, callback);
+                        });
                     }
                 }else{
                     if(isMathEq(resolvedQuery)){
@@ -294,12 +289,16 @@ function processEvent(event) {
                             sendFBMessage(sender,{text: response});  
                         });
                     }else{
-                        console.log("In Else");
-                        let splittedText = splitResponse(responseText);
+                        if(responseText.length>0){
+                            let splittedText = splitResponse(responseText);
 
-                        async.eachSeries(splittedText, (textPart, callback) => {
-                            sendFBMessage(sender, {text: textPart}, callback);
-                        });
+                            async.eachSeries(splittedText, (textPart, callback) => {
+                                sendFBMessage(sender, {text: textPart}, callback);
+                            });
+                        }else{
+                            let respText = "I'm still learning about that myself. As soon as I know, you'll know.";
+                            sendFBMessage(sender, {text: respText});
+                        }
                     }
                 }
 
