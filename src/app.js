1874d6b6bc2b4367078031f41728009d8a86f4ff
Muhammad Awais <jxnenterprises@gmail.com>
2016-07-26 18:13:55 +0500

    changes

--- src/app.js
+++ src/app.js
@@ -316,12 +316,13 @@ function processEvent(event) {
 
                     		if(response.error){
 
-                    			let obj = {};
-	                            obj.title = "Not Found!";
-	                            obj.image_url = "https://eimi.io/img/oopsimage.png";
-	                            obj.subtitle = "No matching location found.";
-	                            elements[0]=obj;
-	                    		sendFBElementMessage(sender,elements);
+                    			// let obj = {};
+	                      //       obj.title = "Not Found!";
+	                      //       obj.image_url = "https://eimi.io/img/oopsimage.png";
+	                      //       obj.subtitle = "No matching location found.";
+	                      //       elements[0]=obj;
+	                    		// sendFBElementMessage(sender,elements);
+	                    		sendFBMessage(sender,{text: "error"});
 
                     		}else{
                             
