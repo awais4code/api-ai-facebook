d8722665d588c8c76a73a90f5feddb0704e571d7
Muhammad Awais <jxnenterprises@gmail.com>
2016-07-26 18:27:13 +0500

    changes

--- src/app.js
+++ src/app.js
@@ -318,7 +318,7 @@ function processEvent(event) {
 
                     			let obj = {};
 	                            obj.title = "Not Found!";
-	                            obj.image_url = "https://eimi.io/img/oopsimage.png";
+	                            obj.image_url = "https://eimi.io/img/oops.jpg";
 	                            obj.subtitle = "No matching location found.";
 	                            elements[0]=obj;
 	                    		sendFBElementMessage(sender,elements);
