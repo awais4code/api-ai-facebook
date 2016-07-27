b9d46720ae0f6274d8cb5e3741c6e5196ea03904
Muhammad Awais <jxnenterprises@gmail.com>
2016-07-27 14:42:24 +0500

    changes

--- src/app.js
+++ src/app.js
@@ -310,7 +310,8 @@ function processEvent(event) {
                         var offset = +5;
                         let d = new Date( new Date().getTime() + offset * 3600 * 1000);
 						sendFBMessage(sender, {text: d.getMilliseconds()});
-                        sendFBMessage(sender, {text: new Date()});
+                        let d1 = new Date();
+                        sendFBMessage(sender, {text: d1.getMilliseconds()});
                     	// let task = parameters.task;
                     	// if(parameters.time.length>0){
                     	// 	let time = parameters.time;
