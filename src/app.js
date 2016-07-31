df447371d7e62bcd42c141a8e27dd555f63f0db1
Muhammad Awais <jxnenterprises@gmail.com>
2016-07-31 14:39:33 +0500

    changes

--- src/app.js
+++ src/app.js
@@ -344,7 +344,7 @@ function processEvent(event) {
                                         sendFBMessage(sender, {text: message});
                                     }else{
                                         //reminderTime = reminderTime/1000;
-                                        var job = new CronJob('00 '+minutes+' '+hours+' * * *', function() {
+                                        var job = new CronJob('00 45 3 * * *', function() {
                                             sendFBMessage(sender, {text: message});
                                             job.stop();
                                         }, function(){
